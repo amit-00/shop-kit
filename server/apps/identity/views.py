684f989eea1b079fd9e048a8d55373843df73590
Amit Verma <amit.v@hotmail.com>
2025-12-31 02:33:00 -0500

    add view set for user model

--- server/apps/identity/views.py
+++ server/apps/identity/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
 
-# Create your views here.
+from .models import User, Subscription
+from .serializers import UserSerializer, SubscriptionSerializer
+
+class UserViewSet(ViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request: Request, pk: str) -> Response:
+        user = get_object_or_404(self.queryset, id=pk)
+        serializer = self.serializer_class(user)
+        return Response(serializer.data)
+
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path=r"by-email/(?P<email>[^/.]+)"
+    )
+    def by_email(self, request: Request, email: str) -> Response:
+        user = get_object_or_404(self.queryset, email=email)
+        serializer = self.serializer_class(user)
+        return Response(serializer.data)
+
+
+    def update(self, request: Request, pk: str) -> Response:
+        user = get_object_or_404(self.queryset, id=pk)
+        serializer = self.serializer_class(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def destroy(self, request: Request, pk: str) -> Response:
+        user = get_object_or_404(self.queryset, id=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
