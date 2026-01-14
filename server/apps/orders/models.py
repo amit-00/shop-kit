ba777715132f90804d2c0935f174035e0b6ff931
Amit Verma <amit.v@hotmail.com>
2026-01-14 15:40:22 -0500

    remove buyer app. User will be buyer by default

--- server/apps/orders/models.py
+++ server/apps/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.model_utils import TimestampedModel
+from apps.sellers.models import Seller
+from apps.identity.models import User
+
+class Order(TimestampedModel):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='orders')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=255, choices=Status.choices, default=Status.PENDING)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
